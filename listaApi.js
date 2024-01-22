16903b396db96fdd23ac729ca85df7c3d63a0e56
Maria Echavarria <maria.echavarria191@pascualbravo.edu.co>
2024-01-22 16:16:19 -0500

    Prueba tecnica con HTML, CSS, JavaScript

--- listaApi.js
+++ listaApi.js
@@ -1,32 +1,29 @@
-$(document).ready(function () {
-    cargarDatos();
-});
-
-function cargarDatos() {
-
-    $.ajax({
-        url: "http://localhost:3000/api/v1/workouts",
-        dataType: "json",
-        success: function (data) {
-            const tablaBody = $("#tablaClientes tbody");
-
-            tablaBody.empty();
-
-            $.each(data.data, function (index, cliente) {
-                const fila = $("<tr>");
-
-                fila.append($("<td>").text(cliente.id));
-                fila.append($("<td>").text(cliente.nombre));
-                fila.append($("<td>").text(cliente.documento));
-                fila.append($("<td>").text(cliente.email));
-                fila.append($("<td>").text(cliente.telefono));
-                fila.append($("<td>").text(cliente.fecha));
-
-                tablaBody.append(fila);
-            });
-        },
-        error: function (error) {
-            console.log("Error al cargar los datos de la API: " + error);
-        }
-    });
-}
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("http://localhost:3000/api/v1/workouts", requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const tablaBody = document.querySelector("#tablaClientes tbody");
+  
+      tablaBody.innerHTML = ''; // Limpiar el cuerpo de la tabla
+  
+      data.data.forEach(cliente => {
+          const fila = document.createElement("tr");
+  
+          fila.innerHTML = `
+          
+              <td>${cliente.nombre}</td>
+              <td>${cliente.documento}</td>
+              <td>${cliente.email}</td>
+              <td>${cliente.telefono}</td>
+              <td>${cliente.fecha}</td>
+          `;
+  
+          tablaBody.append(fila);
+      });
+    })
+    .catch(error => console.log('error', error));
